b29ab407a1f9b8d09b1e84bee7d913240512d569
Emeralda0144 <kwunder014@gmail.com>
2022-09-09 01:24:37 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
--- Lists the privileges for some users on the server
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+--  on your server (in localhost).
+
+-- Account name syntax is 'user_name'@'host_name'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
