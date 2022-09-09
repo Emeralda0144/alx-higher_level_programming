c0fa405408a0c6c60804e3c08fab9c40369e96a1
Emeralda0144 <kwunder014@gmail.com>
2022-09-09 01:22:20 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Lists all privileges of the MySQL Users `user_0d_1`
--- and `user_0d_2` in `localhost`.
+-- Lists the privileges for some users on the server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
