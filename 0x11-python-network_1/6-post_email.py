98a5409cedbdd2e0b3f285e3a8a74007f7871638
Emeralda0144 <kwunder014@gmail.com>
2022-10-24 14:10:21 -0700

    5-hbtn_header.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email address, sends a
-POST request to the passed URL with the email
-as a parameter, and finally displays the body
-of the response.
+"""A  script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response.
 """
-
-from sys import argv
+import sys
 import requests
 
 
 if __name__ == "__main__":
-    payload = {'email': argv[2]}
-    req = requests.post(argv[1], data=payload)
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-    print(req.text)
+    r = requests.post(url, data=value)
+    print(r.text)
